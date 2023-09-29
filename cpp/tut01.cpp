8c8be8817ac01f0dea8bdc040ad2a9cbb96d36f5
Safeer Hyder <safeer.hyder@iba-suk.edu.pk>
2023-09-29 15:52:33 +0200

    this is my second file

---
+++ cpp/tut01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    string name;
+
+    cout << "what is your name ? " << endl;
+    cin >> name ;
+    cout << "How are you Mr." << name << endl; 
+
+    return 0;
+}
